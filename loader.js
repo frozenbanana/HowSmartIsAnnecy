877d23ebad57e33b329aa9074e33c6552a15db04
frozenbanana <henrybergstrom1@gmail.com>
2018-01-16 15:08:27 +0100

    Loading of different charts works

--- loader.js
+++ loader.js
@@ -1,8 +1,7 @@
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
+var dataArray2d_0 = [
     ['Year', 'Sales', 'Expenses'],
     ['2004',  1000,      400],
     ['2005',  1170,      460],
@@ -14,10 +13,29 @@ function drawChart() {
     ['2011',  1180,      1120],
     ['2012',  1230,      340],
     ['2013',  1340,      240]
-  ]);
+  ];
+
+var dataArray2d_1 = [
+    ['Year', 'Sales', 'Killed employees'],
+    ['2004',  108,      40],
+    ['2005',  118,      40],
+    ['2006',  74,       120],
+    ['2007',  108,      50],
+    ['2008',  108,      60],
+    ['2009',  108,      40],
+    ['2010',  118,      40],
+    ['2011',  118,      120],
+    ['2012',  128,      30],
+    ['2013',  138,      20]
+  ];
+
+  var listOfCharts = [dataArray2d_0, dataArray2d_1];
+  var titlesOfCharts = ["Company Performance" , "Another test"]
+function drawChart(index = 0) {
+  var data = google.visualization.arrayToDataTable(listOfCharts[index]);
 
   var options = {
-    title: 'Company Performance',
+    title: titlesOfCharts[index],
     curveType: 'function',
     backgroundColor: '#FAE4D9',
     legend: { position: 'bottom' }
