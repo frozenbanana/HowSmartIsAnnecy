9e431eec66c674bf77d5b6d954bf074d1ebfe9b3
frozenbanana <henrybergstrom1@gmail.com>
2018-01-16 11:49:26 +0100

    Google Charts working

---
+++ loader.js
@@ -0,0 +1,28 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Year', 'Sales', 'Expenses'],
+    ['2004',  1000,      400],
+    ['2005',  1170,      460],
+    ['2006',  660,       1120],
+    ['2007',  1030,      540],
+    ['2008',  1040,      640],
+    ['2009',  1000,      400],
+    ['2010',  1170,      460],
+    ['2011',  1180,      1120],
+    ['2012',  1230,      340],
+    ['2013',  1340,      240]
+  ]);
+
+  var options = {
+    title: 'Company Performance',
+    curveType: 'function',
+    legend: { position: 'bottom' }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+  chart.draw(data, options);
+}
